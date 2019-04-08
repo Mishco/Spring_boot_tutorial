1bbdacf4b89188bd1b370989d913c8463d28069f
Michal <mishcos@gmail.com>
2019-04-08 15:50:02 +0200

    Add request mapping

--- src/test/java/sk/mishco/springboottutorial/ControllerTest.java
+++ src/test/java/sk/mishco/springboottutorial/ControllerTest.java
@@ -1,14 +1,21 @@
 package sk.mishco.springboottutorial;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
+@SpringBootTest
 class ControllerTest {
 
+    @Autowired
+    private HomeController controller;
+
     @Test
-    void nothing() {
-        assertTrue(true);
+    void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 }
+
