c195db50a74ec90016fe04b6ff8bdf9abcdb5d27
Michal <mishcos@gmail.com>
2019-04-08 15:03:23 +0200

    Move from junit4 to junit5

---
+++ src/test/java/sk/mishco/springboottutorial/ControllerTest.java
@@ -0,0 +1,14 @@
+package sk.mishco.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ControllerTest {
+
+    @Test
+    public void nothing() {
+        assertTrue(true);
+    }
+}
